ecf9a0f91535518177b1a22559b4cbeb1969e28a
Ayomide <oyetimehin31@gmail.com>
2024-07-13 19:19:24 +0100

    changes

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,9 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  define: {
+    "process.env": process.env,
+  },
   server: {
     proxy: {
       "/api": {
@@ -18,4 +21,5 @@ export default defineConfig({
       },
     },
   },
+  base: "/",
 });

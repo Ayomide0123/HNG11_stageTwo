2df3df766f0d281c9d4e4cb55cfdc1ea49508536
Ayomide <oyetimehin31@gmail.com>
2024-07-13 20:13:04 +0100

    changes

--- vite.config.js
+++ vite.config.js
@@ -1,21 +1,23 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
-    proxy: {
-      "/api": {
-        target: "https://api.timbu.cloud",
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, ""),
-      },
-      "/extrainfo": {
-        target: "https://api.timbu.cloud",
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/extrainfo/, ""),
-      },
-    },
+    proxy:
+      process.env.NODE_ENV === "development"
+        ? {
+            "/api": {
+              target: "https://api.timbu.cloud",
+              changeOrigin: true,
+              rewrite: (path) => path.replace(/^\/api/, ""),
+            },
+            "/extrainfo": {
+              target: "https://api.timbu.cloud",
+              changeOrigin: true,
+              rewrite: (path) => path.replace(/^\/extrainfo/, ""),
+            },
+          }
+        : {},
   },
 });
